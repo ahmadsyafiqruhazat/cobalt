bd36e0cfaa1db7272ae3dc467220e4807c29c62e
Ahmad Syafiq <luvnenvy@gmail.com>
2020-03-15 11:52:08 +0800

    change lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   rules: {
-    "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
