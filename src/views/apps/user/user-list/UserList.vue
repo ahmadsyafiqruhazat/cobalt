9e85d0510280fb6a3484558693d00f92e1545691
Leslie Tang <tangweejieleslie+github@gmail.com>
2020-04-03 18:45:16 +0800

    fix es lint userlist

--- src/views/apps/user/user-list/UserList.vue
+++ src/views/apps/user/user-list/UserList.vue
@@ -400,7 +400,7 @@ export default {
       moduleUserManagement.isRegistered = true;
     }
     this.$store.dispatch("userManagement/fetchUsers").catch(err => {
-      console.error(err);
+      // console.error(err);
     });
   },
   methods: {
